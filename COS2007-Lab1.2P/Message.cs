b5783b73ea6a13a3fd05460d5b1022f727a254a3
dylanswin <105040962@student.swin.edu.au>
2024-02-29 15:22:53 +1100

    Added code to Message class

--- COS2007-Lab1.2P/Message.cs
+++ COS2007-Lab1.2P/Message.cs
@@ -3,8 +3,17 @@ namespace COS2007_Lab1._2P
 {
 	public class Message
 	{
-		public Message()
+		private string _text;
+
+		public Message(string text)
 		{
+			_text = text;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine(_text);
+			Console.ReadKey();
 		}
 	}
 }
